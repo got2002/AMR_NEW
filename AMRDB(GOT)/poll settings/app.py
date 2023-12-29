b531e9770f63f0ee19a9a41d78f9803d994f102e
Jams-tnp <138769103+Jams-tnp@users.noreply.github.com>
2023-12-29 10:57:12 +0700

    Update app.py

--- AMRDB(GOT)/poll settings/app.py
+++ AMRDB(GOT)/poll settings/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 import cx_Oracle
-import json
 
 app = Flask(__name__)
 
@@ -11,11 +10,16 @@ oracle_host = "192.168.102.192"
 oracle_port = "1521"
 oracle_service = "orcl"
 
-def generate_address_range(start, end):
-    # Generate a range of addresses from start to end
-    address_range = list(range(int(start), int(end) + 1))
-    return address_range
+def insert_address_range_to_oracle(address_range):
+    dsn = cx_Oracle.makedsn(oracle_host, oracle_port, service_name=oracle_service)
 
+    with cx_Oracle.connect(user=oracle_username, password=oracle_password, dsn=dsn) as connection:
+        sql = "INSERT INTO AMR_POLL_RANGE (POLL_CONFIG) VALUES (:address_range)"
+        with connection.cursor() as cursor:
+            cursor.execute(sql, {'address_range': address_range})
+            
+        connection.commit()   
+        
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,25 +29,19 @@ def save_to_oracle():
     try:
         data = request.get_json()
 
-        # Extract start and end addresses from the JSON data
-        start_address = data.get('start', '')
-        end_address = data.get('end', '')
+       # Combine start and end addresses alternately into a single string
+        combined_address_range = ",".join(
+            [data[f'start{i}'] + ',' + data[f'end{i}'] for i in range(1, 6) if data[f'start{i}']])
 
-        # Generate the address range as a list of integers
-        address_range = generate_address_range(start_address, end_address)
 
-        # Convert the list of addresses to a JSON array string
-        poll_config_values = json.dumps(address_range)
+        if not combined_address_range:
+            response = {'status': 'error', 'message': 'Please enter at least one start or end address.'}
+            return jsonify(response)
 
-        dsn = cx_Oracle.makedsn(oracle_host, oracle_port, service_name=oracle_service)
+        # Wrap the combined address range with square brackets to create a JSON array
+        combined_address_range = f"[{combined_address_range}]"
 
-        with cx_Oracle.connect(user=oracle_username, password=oracle_password, dsn=dsn) as connection:
-            # Insert data into Oracle table for the address range using batch insert
-            sql = "INSERT INTO AMR_POLL_RANGE (POLL_CONFIG) VALUES (:poll_config_value)"
-            with connection.cursor() as cursor:
-                cursor.execute(sql, {'poll_config_value': poll_config_values})
-
-            connection.commit()
+        insert_address_range_to_oracle(combined_address_range)
 
         response = {'status': 'success', 'message': 'Data saved successfully'}
     except cx_Oracle.DatabaseError as e:
