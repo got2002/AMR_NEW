38a4bca51d4857bcca8766418179c6e60e2f883b
Nattapong0123 <maoloafour@gmail.com>
2024-01-14 18:21:01 -0800

    Update home.py

--- HOME (Doing)/home.py
+++ HOME (Doing)/home.py
@@ -661,7 +661,13 @@ def billing_data():
                 "CONFIG19",
                 "CONFIG20",
             ]
-            dropped_columns_data = pd.concat([pd.DataFrame(columns=df.columns), pd.DataFrame(columns=columns_to_drop)], axis=1)
+            dropped_columns_data = pd.concat(
+                [
+                    pd.DataFrame(columns=df.columns),
+                    pd.DataFrame(columns=columns_to_drop),
+                ],
+                axis=1,
+            )
             dropped_columns_data = df[["DATA_DATE"] + columns_to_drop].head(1)
             dropped_columns_data[
                 "DATA_DATE"
@@ -1309,6 +1315,292 @@ def logout():
     session.clear()
     # ส่งไปยังหน้าลงชื่อเข้าใช้หลังจากลงชื่อออก
     return redirect(url_for("login"))
+
+
+###############################################
+
+
+def insert_address_range_to_oracle(
+    poll_config, poll_billing, enable_config, enable_billing, evc_type
+):
+    dsn = cx_Oracle.makedsn(hostname, port, service_name)
+
+    with cx_Oracle.connect(
+        user=username, password=password, dsn=dsn
+    ) as connection:
+        with connection.cursor() as cursor:
+            sql_insert = """
+                INSERT INTO AMR_POLL_RANGE (POLL_CONFIG, POLL_BILLING, POLL_CONFIG_ENABLE, POLL_BILLING_ENABLE, EVC_TYPE)
+                VALUES (:1, :2, :3, :4, :5)
+            """
+
+            # Convert enable_config and enable_billing to comma-separated strings
+            enable_config_str = ",".join(map(str, enable_config))
+            enable_billing_str = ",".join(map(str, enable_billing))
+
+            data_to_insert = (
+                poll_config,
+                poll_billing,
+                enable_config_str,
+                enable_billing_str,
+                evc_type,
+            )
+
+            cursor.execute(sql_insert, data_to_insert)
+
+        connection.commit()
+
+
+@app.route("/add_polling_route")
+def add_polling_route():
+    return render_template("add_polling.html")
+
+
+MAX_ADDRESS_LENGTH = 249
+
+
+@app.route("/save_to_oracle", methods=["POST"])
+def save_to_oracle():
+    try:
+        data = request.get_json()
+
+        # Add the following line to define 'evc_type'
+        evc_type = data.get("evc_type", "")
+
+        def validate_address_range(start_key, end_key):
+            start_address = int(data.get(start_key, 0))
+            end_address = int(data.get(end_key, 0))
+
+            if end_address - start_address + 1 > MAX_ADDRESS_LENGTH:
+                raise ValueError(
+                    f"Address range {start_key} - {end_key} exceeds the maximum length of {MAX_ADDRESS_LENGTH}"
+                )
+
+            return start_address, end_address
+
+        combined_address_config = ",".join(
+            [
+                ",".join(map(str, validate_address_range(f"start{i}", f"end{i}")))
+                for i in range(1, 6)
+                if data.get(f"start{i}")
+            ]
+        )
+
+        enable_config = [int(data.get(f"enable{i}", 0)) for i in range(1, 6)]
+
+        combined_address_billing = ",".join(
+            [
+                ",".join(map(str, validate_address_range(f"start{i}", f"end{i}")))
+                for i in range(6, 16)
+                if data.get(f"start{i}")
+            ]
+        )
+
+        enable_billing = [int(data.get(f"enable{i}", 0)) for i in range(6, 16)]
+
+        if not combined_address_config or not combined_address_billing:
+            response = {
+                "status": "error",
+                "message": "Please enter at least one valid start or end address for both configurations.",
+            }
+            return jsonify(response)
+
+        insert_address_range_to_oracle(
+            combined_address_config,
+            combined_address_billing,
+            enable_config,
+            enable_billing,
+            evc_type,
+        )
+
+        response = {"status": "success", "message": "Data saved successfully"}
+    except ValueError as ve:
+        response = {"status": "error", "message": str(ve)}
+    except cx_Oracle.DatabaseError as e:
+        response = {"status": "error", "message": f"Database Error: {e}"}
+    except Exception as e:
+        response = {"status": "error", "message": f"Error: {e}"}
+
+    return jsonify(response)
+
+
+@app.route("/add_mapping_route")
+def add_mapping_route():
+    return render_template("add_mapping.html")
+
+
+@app.route("/submit_form", methods=["POST"])
+def submit_form():
+    cursor = None
+    connection = None
+
+    try:
+        data_list = []
+        for i in range(1, 21):
+            address = request.form[f"address{i}"]
+            description = request.form[f"description{i}"]
+            type_value = request.form.get(f"type_value{i}")
+            evc_type = request.form[f"evc_type{i}"]
+            or_der = request.form[f"or_der{i}"]
+            data_type = request.form[f"data_type{i}"]
+
+            data_list.append(
+                (address, description, type_value, evc_type, or_der, data_type)
+            )
+
+        dsn_tns = cx_Oracle.makedsn(
+            hostname, port, service_name
+        )
+        connection = cx_Oracle.connect(
+            user=username, password=password, dsn=dsn_tns
+        )
+
+        cursor = connection.cursor()
+
+        sql_merge = """
+            MERGE INTO AMR_ADDRESS_MAPPING1 dst
+            USING (
+                SELECT
+                    :address as address,
+                    :description as description,
+                    :type_value as type_value,
+                    :evc_type as evc_type,
+                    :or_der as or_der,
+                    :data_type as data_type
+                FROM dual
+            ) src
+            ON (dst.address = src.address)
+            WHEN MATCHED THEN
+                UPDATE SET
+                    dst.description = src.description,
+                    dst.type_value = src.type_value,
+                    dst.evc_type = src.evc_type,
+                    dst.or_der = src.or_der,
+                    dst.data_type = src.data_type
+            WHEN NOT MATCHED THEN
+                INSERT (
+                    address,
+                    description,
+                    type_value,
+                    evc_type,
+                    or_der,
+                    data_type
+                ) VALUES (
+                    src.address,
+                    src.description,
+                    src.type_value,
+                    src.evc_type,
+                    src.or_der,
+                    src.data_type
+                )
+        """
+
+        cursor.executemany(sql_merge, data_list)
+
+        connection.commit()
+
+        return "บันทึกข้อมูลสำเร็จ"
+    except Exception as e:
+        return f"เกิดข้อผิดพลาด: {str(e)}"
+    finally:
+        if cursor is not None:
+            # ปิด cursor
+            cursor.close()
+
+        if connection is not None:
+            # ปิด connection
+            connection.close()
+
+
+@app.route("/add_actraris_route")
+def add_actraris_route():
+    return render_template("add_actraris.html")
+
+
+@app.route("/new_form", methods=["POST"])
+def submit_new_form():
+    cursor = None
+    connection = None
+
+    try:
+        data_list = []
+        for i in range(1, 18):
+            address = request.form.get(f"address{i}")
+            description = request.form.get(f"description{i}")
+            type_value = request.form.get(f"type_value{i}")
+            evc_type = request.form.get(f"evc_type{i}")
+            or_der = request.form.get(f"or_der{i}")
+            data_type = request.form.get(f"data_type{i}")
+
+            data_list.append(
+                (address, description, type_value, evc_type, or_der, data_type)
+            )
+
+        dsn_tns = cx_Oracle.makedsn(
+            hostname, port, service_name
+        )
+        connection = cx_Oracle.connect(
+            user=username, password=password, dsn=dsn_tns
+        )
+
+        cursor = connection.cursor()
+
+        sql_merge = """
+            MERGE INTO AMR_ADDRESS_MAPPING1 dst
+            USING (
+                SELECT
+                    :address as address,
+                    :description as description,
+                    :type_value as type_value,
+                    :evc_type as evc_type,
+                    :or_der as or_der,
+                    :data_type as data_type
+                FROM dual
+            ) src
+            ON (dst.address = src.address)
+            WHEN MATCHED THEN
+                UPDATE SET
+                    dst.description = src.description,
+                    dst.type_value = src.type_value,
+                    dst.evc_type = src.evc_type,
+                    dst.or_der = src.or_der,
+                    dst.data_type = src.data_type
+            WHEN NOT MATCHED THEN
+                INSERT (
+                    address,
+                    description,
+                    type_value,
+                    evc_type,
+                    or_der,
+                    data_type
+                ) VALUES (
+                    src.address,
+                    src.description,
+                    src.type_value,
+                    src.evc_type,
+                    src.or_der,
+                    src.data_type
+                )
+        """
+
+        cursor.executemany(sql_merge, data_list)
+
+        connection.commit()
+
+        return "บันทึกข้อมูลสำเร็จ"
+    except Exception as e:
+        return f"เกิดข้อผิดพลาด: {str(e)}"
+    finally:
+        if cursor is not None:
+            # ปิด cursor
+            cursor.close()
+
+        if connection is not None:
+            # ปิด connection
+            connection.close()
+
+
+####################################################
 
 
 if __name__ == "__main__":
