9ba27ba0bd8a77724828ee7e1d486f3a947e3a42
got2002 <gohvfjb@gmail.com>
2024-02-04 23:32:00 +0700

    Delete login.py

--- test/app/login.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-from flask import Flask, render_template
-login_bp = Blueprint('login', __name__)
-
-@login_bp.route('/login')
-def login():
-    return render_template("login.html")
