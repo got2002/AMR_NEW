dfc9d76f494dd7accf4919ffdcbdf96559e6e13b
got2002 <gohvfjb@gmail.com>
2024-05-31 10:24:07 +0700

    Delete forms.py

--- HOME (Ultimate)/modules/forms.py
+++
@@ -1,7 +0,0 @@
-# forms.py
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, validators
-
-class RegistrationForm(FlaskForm):
-    # Your registration form fields go here
-    pass
